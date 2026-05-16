693b9dd7805cbc6269f1dc3113e4d8f553ff9692
Satvik-codes <satvikravisharma07@gmail.com>
2026-05-16 23:51:50 +0530

    Enhance comment on theme colors for clarity

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,5 +1,5 @@
 /**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode as we have both dark and light mode features.
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
